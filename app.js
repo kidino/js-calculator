9125f19eb03806e4acda64ce8ec5b06974c4a0a2
Iszuddin Ismail <dino@websegera.my>
2024-05-19 15:14:45 +0800

    Update app.js -- commenting code for explanation

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// calculator needs to capture 2 numbers. we need to know which
+// number we are capturing either the first one or the second.
+// we need to start capturing the second number after the user
+// hit an operator key. we will use these STATUS_ constants to
+// let us know which it is. no1 will store the first number and
+// no2 will store the second number. later will do a mathematical
+// operations with the selector operator.
+
 let no1 = '0';
 let no2 = '0';
 let operator = '';
@@ -6,8 +14,10 @@ const STATUS_FIRST_NUMBER = 0;
 const STATUS_SECOND_NUMBER = 1;
 const STATUS_OPERATOR = 2;
 
+// by default we are capturing the first number.
 let current_status = STATUS_FIRST_NUMBER;
 
+// using event delegation to capture for all buttons
 document.getElementById('buttons').addEventListener(
     'click',
     function(e){
@@ -18,10 +28,8 @@ document.getElementById('buttons').addEventListener(
 
 function handleKey( key ) {
 
-    // document.getElementById('display').innerText 
-    // += key;
-
     switch(key) {
+        // handling number buttons and the dot
         case '0' : 
         case '1' : 
         case '2' : 
@@ -35,15 +43,21 @@ function handleKey( key ) {
         case '.' :
             handle_numbers(key);
             break;
+
+        // handling operator buttons
         case '/':
         case '*':
         case '-':
         case '+':
             handle_operators(key);
             break;
+
+        // handling the Cancel button
         case 'C':
             handle_cancel();
             break;
+
+        // handling the equal button
         case '=':
             handle_calculate();
             break;
@@ -53,6 +67,8 @@ function handleKey( key ) {
 
 function handle_numbers(key) {
 
+    // the dot can only be accepted once. we cannot add another dot
+    // if there is already a dot.
     if((document.getElementById('display').innerText.indexOf('.') > -1)
     && (key == '.'))
      {
@@ -60,6 +76,9 @@ function handle_numbers(key) {
     }
 
     if(current_status == STATUS_FIRST_NUMBER) {
+        // handling if user enters 0 multiple times. a number cannot start with
+        // two zeros like 00. other we will append the number to the back.
+        // currently it is a string, not an integer / float data yet.
         if(no1 == '0') {
             no1 = key;
         } else {
@@ -67,6 +86,8 @@ function handle_numbers(key) {
         }
         update_display(no1);
     } else if(current_status == STATUS_SECOND_NUMBER) {
+        // handling if user enters 0 multiple times. a number cannot start with
+        // two zeros like 00
         if(no2 == '0') {
             no2 = key;
         } else {
@@ -80,11 +101,14 @@ function handle_numbers(key) {
     }
 }
 
+// when an operator button is pressed, we are ready to start
+// capturing the second number into no2.
 function handle_operators(key) {
     current_status = STATUS_SECOND_NUMBER;
     operator = key;
 }
 
+// cancel button -- resets everything to default
 function handle_cancel() {
     current_status = STATUS_FIRST_NUMBER;
     update_display('0')
@@ -93,14 +117,18 @@ function handle_cancel() {
     operator = '';    
 }
 
+// equal button -- we will ca
 function handle_calculate() {
     let result = 0;
+    // parseFloat converts string to float number data type for
+    // mathematical operation
     no1 = parseFloat(no1);
     no2 = parseFloat(no2);
     switch(operator) {
         case '/' : 
             result = no1 / no2;
             break;
+
         case '*' : 
             result = no1 * no2;
             break;
@@ -118,6 +146,7 @@ function handle_calculate() {
     update_display(result)
 }
 
+// updates the display screen
 function update_display( num ) {
     document.getElementById('display').innerText = num;
 }
